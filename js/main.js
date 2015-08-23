9c24be1b62479eafb6150c52c50bcb93c6874d50
Olivia <oliviakim321@gmail.com>
2015-08-23 10:46:20 -0400

    website redesigned

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function() {
+
+    $(".typed-greeting").typed({
+    	strings: ["Hi! I'm Olivia.", "I study Mechatronics engineering.", "I develop software."],
+        typeSpeed: 80,
+        showCursor: true,
+        cursorChar: "|",
+        backDelay: 1000,
+    });
+
+    $('.home-link').click(function() {
+    	var clicked = "";
+    	switch($(this).prop('id')){
+    		case 'projects-link':
+    			clicked = $('#projects-container');
+    			break;
+    		case 'work-link':
+    			clicked = $('#work-container');
+    			break;
+    		case 'contact-link':
+    			clicked = $('#contact-container');
+    			break;
+    		case 'chevron-link':
+    			clicked = $('#about-container');
+    			break;
+    	}
+    	$('html, body').animate({
+		    scrollTop: clicked.offset().top
+		}, 1000);
+    })
+
+});
+
+
+
